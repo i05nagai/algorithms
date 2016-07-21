023a187ab9985853dba7012f77ea964f95122ac7
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-07-21 23:56:33 +0900

    #13:Adds check code for utility functions.

--- ad/main.cpp
+++ ad/main.cpp
@@ -2,7 +2,32 @@
 #include "ad/dual_binary_operator.h"
 #include "ad/dual_unary_operator.h"
 #include "ad/helper_macro.h"
+#include "ad/utility.h"
 #include <boost/numeric/ublas/vector.hpp>
+
+template <typename V>
+struct Functor0 {
+    V operator()(const boost::numeric::ublas::vector<V>& e) const
+    {
+        return e(0) * e(1);
+    }
+};
+
+template <typename V>
+struct Functor1 {
+    V operator()(const boost::numeric::ublas::vector<V>& e) const
+    {
+        return e(0) + e(1) * e(2);
+    }
+};
+
+template <typename V>
+struct Functor2 {
+    V operator()(const boost::numeric::ublas::vector<V>& e) const
+    {
+        return e(0) - e(1);
+    }
+};
 
 int main(int argc, char const* argv[])
 {
@@ -98,6 +123,47 @@ int main(int argc, char const* argv[])
     {
         auto z = algo::ad::cos(d1 + d2);
         DISPLAY_DUAL(z);
+    }
+
+    std::cout << "make_unit_dual" << std::endl;
+    {
+        namespace ublas = boost::numeric::ublas;
+
+        algo::ad::dual<ublas::vector<double> > unitDual
+            = algo::ad::make_unit_dual(3.0, 3, 1);
+        DISPLAY_DUAL(unitDual);
+    }
+    std::cout << "make_vector_dual" << std::endl;
+    {
+        namespace ublas = boost::numeric::ublas;
+
+        ublas::vector<double> value(3, 1.0);
+        ublas::vector<algo::ad::dual<ublas::vector<double> > > vectorDual 
+            = algo::ad::make_vector_dual(value);
+        DISPLAY_VECTOR_DUAL(vectorDual);
+    }
+    std::cout << "apply_element" << std::endl;
+    {
+        typedef algo::ad::dual<ublas::vector<double> > dual_type;
+        typedef ublas::vector<dual_type> argument_type;
+        typedef std::function<dual_type (const argument_type&)> functor_type;
+
+        ublas::vector<double> values(3);
+        values(0) = 1.0;
+        values(1) = 2.0;
+        values(2) = 3.0;
+        ublas::vector<dual_type> x = algo::ad::make_vector_dual(values);
+
+        std::vector<functor_type> functors;
+        Functor0<algo::ad::dual<ublas::vector<double> > > functor0;
+        Functor1<algo::ad::dual<ublas::vector<double> > > functor1;
+        
+        functors.push_back(functor0);
+        functors.push_back(functor1);
+
+        DISPLAY_VECTOR_DUAL(x);
+        auto y = algo::ad::apply_element(functors, x);
+        DISPLAY_VECTOR_DUAL(y);
     }
     return 0;
 }
