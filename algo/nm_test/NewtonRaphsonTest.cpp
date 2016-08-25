f4c609d4f3261c15e83fe3159df6b30d811ea548
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-08-26 08:01:31 +0900

    #32:Adds nm_test.

---
+++ algo/nm_test/NewtonRaphsonTest.cpp
@@ -0,0 +1,46 @@
+#include "nm_test/NewtonRaphsonTest.h"
+#include "nm_test/TestData.h"
+#include "test_util/TestData.h"
+#include "test_util/gtest_helper_macro.h"
+#include "nm/NewtonRaphson.h"
+#include "utility/debug_macro.h"
+#include <boost/numeric/ublas/detail/vector_assign.hpp>
+
+namespace algo { namespace nm_test {
+    namespace ublas = boost::numeric::ublas;
+
+    NewtonRaphsonTest::NewtonRaphsonTest() 
+    {
+    }
+
+    NewtonRaphsonTest::~NewtonRaphsonTest() 
+    {
+    }
+
+    void NewtonRaphsonTest::SetUp()
+    {
+    }
+
+    void NewtonRaphsonTest::TearDown()
+    {
+    }
+
+    TEST_F(NewtonRaphsonTest, doSolveTest)
+    {
+        nm::INewtonMethod::function_type f 
+            = TestData::getMultiValuedSeveralVariablesFunction0();
+        nm::INewtonMethod::derivative_type derivative 
+            = TestData::getMatrixValuedSeveralVariablesDerivative0();
+        const std::size_t maxIteration = 50;
+        const double tolerance = 2E-32;
+        nm::NewtonRaphson target(f, derivative, maxIteration, tolerance);
+
+        ublas::vector<double> initialValue 
+            = test_util::TestData::getRandomNumbers(2);
+        const ublas::vector<double> actual = target.solve(initialValue);
+
+        ublas::vector<double> expect(2, 0.0);
+        EXPECT_VECTOR_NEAR(expect, actual, 2E-15);
+    }
+} } // namespace algo { namespace nm_test {
+
---
+++ algo/nm_test/TestData.cpp
@@ -0,0 +1,54 @@
+#include "nm_test/TestData.h"
+
+namespace algo { namespace nm_test {
+    namespace ublas = boost::numeric::ublas;
+
+    std::function<
+        ublas::vector<double>
+            (const ublas::vector<double>& x)>
+    TestData::getMultiValuedSeveralVariablesFunction0()
+    {
+        return [](const ublas::vector<double>& x)
+        {
+            assert(x.size() == 2);
+            ublas::vector<double> y(x.size());
+            y(0) = x(0) * x(0);
+            y(1) = x(1) * x(1);
+            return y;
+        };
+    }
+
+    std::function<
+        ublas::matrix<double>
+            (const ublas::vector<double>& x)>
+    TestData::getMatrixValuedSeveralVariablesDerivative0()
+    {
+        namespace ublas = boost::numeric::ublas;
+        return [](const ublas::vector<double>& x)
+        {
+            assert(x.size() == 2);
+            ublas::matrix<double> y(x.size(), x.size());
+            y(0, 0) = 2.0 * x(0);
+            y(0, 1) = 0.0;
+            y(1, 0) = 0.0;
+            y(1, 1) = 2.0 * x(1);
+            return y;
+        };
+    }
+
+    boost::numeric::ublas::vector<double> 
+    TestData::getVector0()
+    {
+        ublas::vector<double> x(2);
+
+        return x;
+    }
+
+    ublas::matrix<double>
+    TestData::getRandomMatrix0()
+    {
+        ublas::matrix<double> matrix(2, 2);
+
+        return matrix;
+    }
+} } // namespace algo { namespace nm_test {
