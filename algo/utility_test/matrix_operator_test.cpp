c18c64d34a8d8a4647836e381f2a16b61053b285
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-08-26 08:01:03 +0900

    #32:Modifies matrix_operator_test.

--- algo/utility_test/matrix_operator_test.cpp
+++ algo/utility_test/matrix_operator_test.cpp
@@ -1,5 +1,8 @@
 #include "utility_test/matrix_operator_test.h"
 #include "utility/matrix_operator.h"
+#include "utility/debug_macro.h"
+#include "test_util/TestData.h"
+#include "test_util/gtest_helper_macro.h"
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/detail/matrix_assign.hpp>
 
@@ -24,13 +27,13 @@ namespace algo { namespace utility_test {
 
     TEST_F(MatrixOperatorTest, invert)
     {
-        ublas::matrix<double> matrix(2, 2);
+        ublas::matrix<double> matrix 
+            = test_util::TestData::getRandomMatrix(2, 2);
         ublas::matrix<double> inverseMatrix(2, 2);
         
         utility::invert(matrix, inverseMatrix);
         ublas::matrix<double> identity = ublas::prod(matrix, inverseMatrix);
-        ASSERT_TRUE(ublas::detail::expression_type_check(
-            ublas::identity_matrix<double>(2),
-            identity));
+        
+        EXPECT_MATRIX_DOUBLE_EQ(ublas::identity_matrix<double>(2), identity);
     }
 } } // namespace algo { namespace utility_test {
