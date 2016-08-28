c17c54da2b17e617dce7f793e7efa76b4158c03b
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-08-28 23:01:05 +0900

    #1:Deletes not existing files in nm.cmake

--- nm.cmake
+++ nm.cmake
@@ -1,10 +1,8 @@
 LIST (APPEND nm_SOURCES
     algo/nm/INewtonMethod.cpp
-    algo/nm/LevenbergMarquardt.cpp
     algo/nm/NewtonRaphson.cpp
 )
 LIST (APPEND nm_HEADERS
     algo/nm/INewtonMethod.h
-    algo/nm/LevenbergMarquardt.h
     algo/nm/NewtonRaphson.h
 )
